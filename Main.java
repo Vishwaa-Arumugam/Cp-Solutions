0786a567bc8ac2d9eecdd5de970f4c53cbe05ad4
Vishwaa-Arumugam <127880126+Vishwaa-Arumugam@users.noreply.github.com>
2024-08-10 18:45:04 +0530

    intervals

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals = {{1,4},{4,5}};
+        Mergeintervals mint = new Mergeintervals();
+        mint.function(intervals);
+    }
+
+}
